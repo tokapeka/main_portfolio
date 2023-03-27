beca7f60136485dd47ae102dc0dffb8f254f217d
Anton Plesca <anton.plesca@yahoo.com>
2023-03-27 16:22:27 -0600

    skills

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from './App';
 import './index.css';
+import 'react-tooltip/dist/react-tooltip.css';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
