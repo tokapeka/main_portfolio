ddb85980d6d4e05f6c4c1e64d119235e8876eebb
Anton Plesca <anton.plesca@yahoo.com>
2023-03-29 23:06:29 -0600

    contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { About, Footer, Header, Skills, Work } from './container';
 import { Navbar } from './components';
 import './App.scss'
 
@@ -13,7 +13,7 @@ const App = () => {
       <About />
       <Work />
       <Skills />
-      <Testimonial />
+      {/* <Testimonial /> */}
       <Footer />
     </div>
   );
